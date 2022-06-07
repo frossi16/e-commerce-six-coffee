6cd4c82200503a8eaf4c2301849e2cdb47881563
Fabricio Rossi <frprogra16@gmail.com>
2022-06-07 19:26:13 -0300

    users creados

---
+++ api-back/models/index.js
@@ -0,0 +1,16 @@
+// const { Schema, model } =require( "mongoose");
+
+// const UserSchema = new Schema(
+//   {
+//     name: { type: String, trim: true },
+//     // email: { type: String, required: true, unique: true, trim: true },
+//     // password: { type: String, required: true },
+//     // date: { type: Date, default: Date.now },
+//   },
+//   {
+//     timestamps: true,
+//     versionKey: false,
+//   }
+// );
+
+// module.exports = model('user',UserSchema);
--- api-back/server.js
+++ api-back/server.js
@@ -1,9 +1,33 @@
 const express = require('express')
+const routes = express.Router()
 const cors = require('cors')
 const app = express()
-
+// const mongoose = require('mongoose')
+require('./db')
+// const User = require('./models')
 app.use(cors())
+app.use(express.json())
+// const user2 = mongoose.model('user',User )
 
+// app.post('/', (req,res,next)=>{
+//   // // console.log(req.body + '   req body')
+//   //  const newUser = new User(req.body);
+//   // // console.log(newUser + '          newuser')
+//   // await newUser.save();
+//     // res.send(newUser)
+
+  
+
+//   User.create(req.body)
+//   .then(asd => res.send(asd))
+//   .catch(err => console.log(err))
+// })
+
+
+
+// db.sync({force: true}).then(()=>
 app.listen(3030, () => {
     console.log("Servidor corriendo en el puerto http://localhost:3030");
-  });
+  })
+  // )
+
