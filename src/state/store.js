e9925c9cee5ff2793a8b546b79acd913e6641792
arieliten85 <arieliten85@gmail.com>
2022-06-09 18:55:07 -0300

    redux_store

---
+++ src/state/store.js
@@ -0,0 +1,10 @@
+//CONFIG STORE ARIEL
+import { configureStore } from "@reduxjs/toolkit";
+import logger from "redux-logger";
+
+const store = configureStore({
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  reducer: {},
+});
+
+export default store;
