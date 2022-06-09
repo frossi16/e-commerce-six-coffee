9e1e56af4279c6864fa59b1fe9adbbd82df740eb
arieliten85 <arieliten85@gmail.com>
2022-06-09 16:51:19 -0300

    agrege a al db rol

--- api-back/models/User.js
+++ api-back/models/User.js
@@ -1,39 +1,51 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const UserSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-
-  lastNme: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-
-  email: {
-    type: String,
-    trim: true,
-    lowercase: true,
-    unique: true,
-    validate: {
-      validator: function (v) {
-        return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
-      },
-      message: "Please enter a valid email",
+const UserSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    required: [true, "Email required"],
+
+    lastNme: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      unique: true,
+      validate: {
+        validator: function (v) {
+          return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+        },
+        message: "Please enter a valid email",
+      },
+      required: [true, "Email required"],
+    },
+    roles: [
+      {
+        ref: "Role",
+        type: Schema.Types.ObjectId,
+      },
+    ],
   },
-});
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+); 
 
 const User = mongoose.model("User", UserSchema);
 
